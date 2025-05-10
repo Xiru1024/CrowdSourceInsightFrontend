a1c515847d9dd978b42410f305c19db87c89138a
Xiru Chen <xiruchen1024@gmail.com>
2025-05-10 19:41:20 +0300

    Add map dependency

--- crowdSourcedInsights/src/app/components/map/map.component.ts
+++ crowdSourcedInsights/src/app/components/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,  ViewChild, ElementRef, AfterViewInit, OnInit } from '@angular/core';
+import { Browser, Map, map, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -6,6 +7,31 @@ import { Component } from '@angular/core';
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
-export class MapComponent {
+export class MapComponent implements OnInit, AfterViewInit {
+  @ViewChild('map')
+  private mapContainer!: ElementRef<HTMLElement>;
 
+  constructor() { 
+  }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    const initialState = { lng: 11, lat: 49, zoom: 4 };
+
+    const lefletMap: Map = map(this.mapContainer.nativeElement).setView([initialState.lat, initialState.lng], initialState.zoom);
+
+    console.log("lefletMap: ", lefletMap);
+    const isRetina = Browser.retina;
+    const baseUrl = "https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey={apiKey}";
+    const retinaUrl = "https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}@2x.png?apiKey={apiKey}";
+    
+    tileLayer(isRetina ? retinaUrl : baseUrl, {
+      attribution: 'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | <a href="https://openmaptiles.org/" target="_blank">© OpenMapTiles</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap</a> contributors',
+      apiKey: '50023782157049cd9b7e6f74ef079e5d',
+      maxZoom: 20,
+      id: 'osm-bright',
+    } as any).addTo(lefletMap);
+  }
 }
